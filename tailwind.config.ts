0b30870ffa0d9ea43c21f6402517319f5f97604b
akash02ab <akash02.ab@gmail.com>
2024-01-23 00:38:21 +0530

    chore: add colors at tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
@@ -7,12 +8,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'dark-navy': '#020c1b',
+      'navy': '#0a192f',
+      'light-navy': '#112240',
+      'lightest-navy': '#233554',
+      'navy-tint': '#020c1bb3',
+      'dark-slate': '#495670',
+      'slate': '#8892b0',
+      'light-slate': '#a8b2d1',
+      'lightest-slate': '#ccd6ff6',
+      'white': '#e6f1ff',
+      'green': '#64ffda',
+      'green-tint': '#64ffda1a',
+      'pink': '#f57dff',
+      'blue': '#57cbff',
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      screens: {
+        xxs: '320px',
+        xs: '480px',
+        ...defaultTheme.screens,
+      }
     },
   },
   plugins: [],
