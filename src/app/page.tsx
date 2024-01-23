f161294601e6f2a15fbfc43cd36753f7ce9d2681
akash02ab <akash02.ab@gmail.com>
2024-01-23 20:27:28 +0530

    feat: add logo on nav component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Nav from "../components/nav";
 
 export default function Home() {
   return (
     <main className="dark:bg-navy h-dvh">
+      <Nav />
     </main>
   );
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+import Logo from "../../public/logo.svg";
+
+export default function Nav() {
+  return (
+    <div>
+      <Logo
+        className="fill-green"
+        width="80"
+        height="40"
+      />
+    </div>
+  )
+}
