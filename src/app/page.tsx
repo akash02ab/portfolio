c7ea4b81c18c87a1a4a94b55b5198afeaf264e4b
akash02ab <akash02.ab@gmail.com>
2024-01-23 22:19:08 +0530

    style: add padding to container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
 import Nav from "../components/nav";
 
 export default function Home() {
+  const containerStyle = `
+    dark:bg-navy
+    2xl:px-12
+    lg:px-10
+    xxs:px-6
+    h-dvh
+  `;
+  
   return (
-    <main className="dark:bg-navy h-dvh">
+    <main className={containerStyle}>
       <Nav />
     </main>
   );
