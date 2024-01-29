9733f94a33b43c58badc8542314b9b16ad64d0cb
akash02ab <akash02.ab@gmail.com>
2024-01-29 19:28:26 +0530

    chore: add project data with title, description and links

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -34,7 +34,7 @@ const rawJobData = [
   },
   {
     "title": "React Developer",
-    "company":"Nkemtasoft",
+    "company": "Nkemtasoft",
     "range": "Oct 2021",
     "url": "https://www.linkedin.com/company/nkemtasoft/",
     "control": [
@@ -44,7 +44,7 @@ const rawJobData = [
   },
   {
     "title": "React Developer",
-    "company":"Metanoia Technologies",
+    "company": "Metanoia Technologies",
     "range": "Sept 2021",
     "url": "https://metanoiatechnologies.com/",
     "control": [
@@ -52,6 +52,89 @@ const rawJobData = [
       "Tackled bugs related to data fetching from Firebase, involving a combination of frontend and backend skills to identify and rectify issues in the data retrieval process."
     ]
   }
-]
+];
 
-export { rawJobData };
+const projectData = [
+  {
+    "title": "Everyone Answers",
+    "description": "Develop an app for live online classes, enabling teachers to quickly gather responses from students. Teachers can create sessions, add students, and share a unique link. When students provide answers through the portal, the responses instantly appear on the teacher's dashboard.",
+    "tech_stack": ["React", "Redux", "Firebase", "Material UI"],
+    "github": "https://github.com/akash02ab/everyone-answers",
+    "url": "https://everyone-answers.netlify.app/"
+  },
+  {
+    "title": "Movies Hub",
+    "description": "A movie website showcasing the latest releases with detailed information including plot summaries, genres, and links to trailers.",
+    "tech_stack": ["React", "TMDB API", "CSS"],
+    "github": "https://github.com/akash02ab/movies-website",
+    "url": "https://the-movies-hub.netlify.app/"
+  },
+  {
+    "title": "Memory Game",
+    "description": "A 4x4 grid with randomly placed fruit pairs. Players click on cells to reveal fruit and aim to match pairs consecutively. Matched pairs lock, and the goal is to uncover all pairs with the fewest moves possible.",
+    "tech_stack": ["React", "CSS"],
+    "github": "https://github.com/akash02ab/memory-game",
+    "url": "https://memorize-fruits.netlify.app/"
+  },
+  {
+    "title": "Quiz App",
+    "description": "Create a basic quiz app where users can answer multiple-choice questions. The app displays their progress, showing the current score, as well as the minimum and maximum possible scores at any moment.",
+    "tech_stack": ["React", "Bootstrap", "Styled Components", "Jest"],
+    "github": "https://github.com/akash02ab/outlier-quiz-challenge",
+    "url": "https://quiz-app-by-akash.netlify.app/"
+  },
+  {
+    "title": "Crypto Trading Simulator",
+    "description": "A Crypto Trading Simulator using CoinGecko API. Users can buy/sell coins and track their transaction history within the platform.",
+    "tech_stack": ["React", "CoinGecko API", "CSS"],
+    "github": "https://github.com/akash02ab/crypto-trading-platform",
+    "url": "https://crypto-trading-simulation.netlify.app/"
+  },
+  {
+    "title": "Tic Tac Toe",
+    "description": "A Tic-Tac-Toe game with a unique feature – users can revisit and undo their previous moves.",
+    "tech_stack": ["React", "CSS"],
+    "github": "https://github.com/akash02ab/tic-tac-toe",
+    "url": "https://tictactoe-by-akash.netlify.app/"
+  },
+  {
+    "title": "Pexels Curated",
+    "description": "Search any image from Pexels API",
+    "tech_stack": ["React", "Pexels API", "CSS"],
+    "github": "https://github.com/akash02ab/pexels-image-search",
+    "url": "https://pexels-curated.netlify.app/"
+  },
+  {
+    "title": "Color Picker",
+    "description": "Choose from color picker or generate random color",
+    "tech_stack": ["HTML", "CSS", "JavaScript"],
+    "github": "https://github.com/akash02ab/color-picker",
+    "url": "https://hexa-color-picker.netlify.app/"
+  },
+  {
+    "title": "Trello Board",
+    "description": "Build a basic version of a Trello board. It has three boards: Todo, Doing, and Done. Users can easily move their tasks between these boards using drag-and-drop",
+    "tech_stack": ["HTML", "CSS", "Javascript"],
+    "github": "https://github.com/akash02ab/trello-layout",
+    "url": "https://noob-trello-board.netlify.app/"
+  },
+  {
+    "title": "Simon Says",
+    "description": "Simon Says is a memory and pattern recognition game. In the game, a sequence of colors, sounds, or actions is presented to the player by 'Simon' (the game). The player then needs to replicate the sequence by repeating the colors in the correct order.",
+    "tech_stack": ["HTML", "CSS", "JavaScript"],
+    "github": "https://github.com/akash02ab/simon-says",
+    "url": "https://simon-says-v1.netlify.app/"
+  },
+  {
+    "title": "Random Advice Generator",
+    "description": "A minimal looking card that displays random advice fetched from Advice Slip API",
+    "tech_stack": ["HTML", "CSS", "JavaScript"],
+    "github": "https://github.com/akash02ab/random-advice-generator/",
+    "url": "https://random-advise-generator.netlify.app/"
+  },
+];
+
+export {
+  rawJobData,
+  projectData
+};
