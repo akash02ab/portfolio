23cbf2f23f482404b59f0f32d061211b95687fb9
akash02ab <akash02.ab@gmail.com>
2024-02-02 10:17:21 +0530

    update: add upwork link to say hello button

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -3,18 +3,20 @@ import { availStyle, contactWrapper } from "../styles/contact.style";
 import Title from "../title";
 
 export default function Contact() {
-  const email = "akash02.ab@gmail.com";
-  
   return (
     <section id="contact" className="max-w-5xl">
       <Title index="04">Get In Touch</Title>
       <div className={contactWrapper}>
         <p className={availStyle}>
-          I am actively exploring new opportunities, and my inbox is open for any discussions or messages. I&apos;m eager to connect with you on various topics, so please feel free to reach out. I&apos;ll make sure to respond promptly and look forward to the chance of connecting with you!
+          I am actively exploring new opportunities, feel free to reach me out at Upwork for any discussions or messages. I&apos;m eager to connect with you on various topics, so please feel free to reach out. I&apos;ll make sure to respond promptly and look forward to the chance of connecting with you!
         </p>
+        <p className={availStyle}><span className="text-pink">Note:</span> If you&apos;re from Upwork then please reach me out via Upwork Messasges as per Upwork&apos;s Terms and Service.</p>
         <Button>
-          <a href={`mailto:${email}`}>
-            Say Hello
+          <a
+            href="https://www.upwork.com/freelancers/~01a88d2143d5968304"
+            target="_blank"
+            rel="noreferrer">
+            Say Hello!
           </a>
         </Button>
       </div>
